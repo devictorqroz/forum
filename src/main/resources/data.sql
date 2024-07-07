5001696aeead4192fb4388b8f01969bfd142912b
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-07-07 01:49:01 -0300

    feat: Adds Pageable with Sort : GET topicos/lista

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,7 @@ INSERT INTO CURSO(nome, subcategoria) VALUES('HTML 5', 'Front-end');
 
 INSERT INTO TOPICO(titulo, mensagem, data, status, curso_id, dono_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', '1', '1');
 INSERT INTO TOPICO(titulo, mensagem, data, status, curso_id, dono_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', '1', '1');
-INSERT INTO TOPICO(titulo, mensagem, data, status, curso_id, dono_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', '2', '1');
+INSERT INTO TOPICO(titulo, mensagem, data, status, curso_id, dono_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 22:00:00', 'NAO_RESPONDIDO', '1', '1');
+INSERT INTO TOPICO(titulo, mensagem, data, status, curso_id, dono_id) VALUES('Dúvida 4', 'Beans', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', '1', '1');
 
 
